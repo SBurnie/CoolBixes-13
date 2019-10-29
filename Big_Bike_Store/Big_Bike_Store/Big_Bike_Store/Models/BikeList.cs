b7cd41c85ad3033233ecdbba56e13f28136dbf50
jReinhart01 <47505874+jReinhart01@users.noreply.github.com>
2019-10-29 11:55:41 -0400

    IntegrationProjectBaseline
    Josh's baseline

---
+++ Big_Bike_Store/Big_Bike_Store/Big_Bike_Store/Models/BikeList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Big_Bike_Store.Models
+{
+    public class BikeList : AdventureWorksLT2017gr1Context
+    {
+        public string ProductModel { get; set; }
+
+        public string Description { get; set; }
+
+        public int ProductModelId { get; set; }
+    }
+}
---
+++ Big_Bike_Store/Big_Bike_Store/Models/BikeList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Big_Bike_Store.Models
+{
+    public class BikeList : AdventureWorksLT2017gr1Context
+    {
+
+        public string ProductModel { get; set; }
+
+        public string Description { get; set; }
+
+        public int ProductModelId { get; set; }
+    }
+}
